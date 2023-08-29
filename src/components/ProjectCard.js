183d6e31dc51acb1460c34b09ede0efa207a74bb
Narayan Aryal <aryal.coding@gmail.com>
2023-08-29 17:07:08 +1000

    code optimised

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,33 +1,27 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { BsRocketTakeoffFill } from "react-icons/bs";
 
 export const ProjectCard = ({ title, description, imgUrl, github, live }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="project" height="70px" width="50px"/>
+        <img src={imgUrl} alt="project" height="70px" width="50px" />
         <div className="proj-txtx text-light">
           <h4 className="p-2">{title}</h4>
           <span>{description}</span>
 
           <Row className="p-3">
             <Col>
-              <Button variant="none">
-                {" "}
-                <a href={github} target="_blank" rel="noreferrer">
-                  <i className="fa-brands fa-github text-light h3"> </i>
-                </a>
-              </Button>
+              <a href={github} target="_blank" rel="noreferrer">
+                <i className="fa-brands fa-github text-light h3"> </i>
+              </a>
             </Col>
             <Col>
-              <Button variant="none">
-                <a href={live} target="_blank" rel="noreferrer">
-                  <div className="h3 ">
-                    {/* <i className="fa-solid fa-right-from-bracket text-light "></i> */}
-<BsRocketTakeoffFill color="white"/>
-                  </div>
-                </a>
-              </Button>
+              <a href={live} target="_blank" rel="noreferrer">
+                <div className="h3 ">
+                  <BsRocketTakeoffFill color="white" />
+                </div>
+              </a>
             </Col>
           </Row>
         </div>
